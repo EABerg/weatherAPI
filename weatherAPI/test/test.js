a4ed745f9e4222afa7d229982e7dae634879002d
Erwin van den Berg <erwinvdberg@me.com>
2021-10-27 14:50:54 +0200

    Edit of tests

--- weatherAPI/test/test.js
+++ weatherAPI/test/test.js
@@ -29,7 +29,7 @@ describe("Check status of source", function () {
             });
     });
 
-    it("Header should contain version " + sourceFile.sourceVersion, function (done) {
+    it("header should contain version " + sourceFile.sourceVersion, function (done) {
 
         // calling the source api and check for the version
         source
@@ -43,23 +43,6 @@ describe("Check status of source", function () {
             });
     });
 
-});
-
-describe("Check the sceme of the source", function () {
-
-    it("should return a 401 page", function (done) {
-
-        // calling the source api and check if it's not down
-        source
-            .get("/")
-            .expect("Content-type", /json/)
-            .expect(401) // THis is HTTP response
-            .end(function (err, res) {
-                // HTTP status should be 401
-                res.status.should.equal(401);
-                done();
-            });
-    });
 });
 
 describe("Check status of API", function () {
@@ -94,9 +77,9 @@ describe("Check status of API", function () {
 
 });
 
-describe("Check the information from the API", function () {
+describe("Information from API", function () {
 
-    it("Return from homepage should be text", function (done) {
+    it("should return from homepage should be text", function (done) {
 
         // calling home page api
         server
@@ -110,7 +93,7 @@ describe("Check the information from the API", function () {
             });
     });
 
-    it("Return from currenttemp page should be number", function (done) {
+    it("should return from currenttemp page should be number", function (done) {
 
         // calling currenttemp page api
         server
@@ -119,7 +102,6 @@ describe("Check the information from the API", function () {
             .expect(200) // THis is HTTP response
             .end(function (err, res) {
                 // HTTP status should be 200
-                console.log(res.body.currentTemp);
                 res.body.currentTemp.should.be.type('number');
                 done();
             });
